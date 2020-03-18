24f074faad3e6b06c4351f77c39e85a848c45751
iqbalabdurrazaq <mancunianmerchant816@gmail.com>
2020-03-17 17:59:39 -0700

    homeController (add/delete role,permission and rolehaspermission)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // Role::create(['name' => 'listing']); // add role in table role
+        // Permission::create(['name' => 'writte post']); // add permission in table permission
+        // $permission = Permission::create(['name'=>'edit post']); // add permission in table permission
+        // $role = Role::findById(1);
+        // $permission = Permission::findById(2);
+        // $role->givePermissionTo($permission); // add idpermission and idrole(based on $role->id) in table role has permission
+        // $permission->assignRole($role); // add idrole and idpermission(based on $permission->id) in table role has permission
+        // $permission->removeRole($role); // remove idpermission and idrole in table role has permission
+        // $role->revokePermissionTo($permission);
         return view('home');
     }
 }
